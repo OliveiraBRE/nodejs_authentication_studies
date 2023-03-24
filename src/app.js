876488d171cda4dc5b161ff80c7870e22b162138
OliveiraBRE <bruno.r.e.oliveira@gmail.com>
2023-03-24 16:37:38 -0300

    fix: express.urlencoded - extended from false to true

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const loginRoute = require('./routes/loginRoute');
 
 const app = express();
 
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 app.use('/login', loginRoute);
