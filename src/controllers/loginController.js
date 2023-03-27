6af834082d90240af79ef166a25ebd6b0c319e8e
OliveiraBRE <bruno.r.e.oliveira@gmail.com>
2023-03-26 22:34:50 -0300

    feature: nav routes SignIn and Logout

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,7 +1,15 @@
 module.exports = {
+  home(req, res, next) {
+    res.render('home', {
+      pageTitle: 'Home',
+      isLoggedIn: true
+    });
+  },
+
   getSignup(req, res, next) {
     res.render('signup', {
-      pageTitle: 'Login'
+      pageTitle: 'SignUp',
+      isLoggedIn: false
     });
   }
 }
--- src/routes/loginRoute.js
+++ src/routes/loginRoute.js
@@ -2,5 +2,6 @@ const route = require('express').Router();
 const loginController = require('../controllers/loginController');
 
 route.get('/signup', loginController.getSignup);
+route.get('/home', loginController.home);
 
 module.exports = route;
