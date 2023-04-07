549348c7114a3a89de04bbb3ca66e673956b3ee4
OliveiraBRE <bruno.r.e.oliveira@gmail.com>
2023-04-07 01:47:58 -0300

    fature: vad=lidators

--- src/routes/loginRoute.js
+++ src/routes/loginRoute.js
@@ -1,9 +1,38 @@
 const route = require('express').Router();
+const { check, body } = require('express-validator');
 const isAuth = require('../middleware/is-auth');
 const loginController = require('../controllers/loginController');
+const User = require('../models/User');
 
 route.get('/signup', loginController.getSignup);
-route.post('/signup', loginController.createUser);
+route.post('/signup',
+  [
+    check('email')
+      .isEmail()
+      .withMessage('Email inválido')
+      .custom((value, { req }) => {
+        return User.findOne({ email: value })
+          .then(userDoc => {
+            if (userDoc) {
+              return Promise.reject('Email já cadastrado');
+            }
+          })
+      }),
+
+    body('password', 'Ao menos 6 caracteres')
+      .isLength({ min: 6 }),
+
+    body('confirmPassword')
+      .custom((value, { req }) => {
+        if (value !== req.body.password) {
+          throw new Error('Senha não confere com a confirmação de senha');
+        }
+
+        return true;
+      })
+  ]
+  , loginController.createUser);
+
 route.get('/signin', loginController.getSignin);
 route.post('/login', loginController.postLogin);
 route.get('/logout', loginController.getLogout);
